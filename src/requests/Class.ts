1534b4d5ee1c749563e1b50d53ede1eea5645357
Lev Kuzmin <levbaladev@gmail.com>
2024-11-29 13:03:07 +0530

    Add integration test for the "getPosts2" method

--- src/requests/Class.ts
+++ src/requests/Class.ts
@@ -2,30 +2,44 @@ import axios, { AxiosInstance } from 'axios';
 import https from 'https';
 
 export class Class {
-    private readonly axiosClient: AxiosInstance;
+  private readonly axiosClient: AxiosInstance;
 
-    /**
-     * Constructs an instance of Class.
-     * @param {string} BASE_URL The base URL of the Class API.
-     * @param {number} TIMEOUT (sec) The base timeout for Class API requests.
-     */
-    constructor(BASE_URL: string, TIMEOUT: number) {
-        const agent = new https.Agent();
-        this.axiosClient = axios.create({ baseURL: BASE_URL, timeout: TIMEOUT * 1000, httpsAgent: agent });
-    }
+  /**
+   * Constructs an instance of Class.
+   * @param {string} BASE_URL The base URL of the Class API.
+   * @param {number} TIMEOUT (sec) The base timeout for Class API requests.
+   */
+  constructor(BASE_URL: string, TIMEOUT: number) {
+    const agent = new https.Agent();
+    this.axiosClient = axios.create({
+      baseURL: BASE_URL,
+      timeout: TIMEOUT * 1000,
+      httpsAgent: agent,
+    });
+  }
 
-    /**
-     * Fetches the /posts/1 data from the Class API.
-     * @returns A promise that resolves to the request.
-     * @throws {Error} Will throw an error if the request fails.
-     */
-    async getPosts1() {
-        try {
-            const response = await this.axiosClient.get(`/posts/1`);
-            return response.data;
-        } catch (error) {
-            console.error('[class][getPosts1] Error:', error.message);
-            throw new Error(error.message);
-        }
+  /**
+   * Fetches the /posts/1 data from the Class API.
+   * @returns A promise that resolves to the request.
+   * @throws {Error} Will throw an error if the request fails.
+   */
+  async getPosts1() {
+    try {
+      const response = await this.axiosClient.get(`/posts/1`);
+      return response.data;
+    } catch (error) {
+      console.error('[class][getPosts1] Error:', error.message);
+      throw new Error(error.message);
     }
+  }
+
+  async getPosts2() {
+    try {
+      const response = await this.axiosClient.get(`/posts/2`);
+      return response.data;
+    } catch (error) {
+      console.error('[class][getPosts2] Error:', error.message);
+      throw new Error(error.message);
+    }
+  }
 }
