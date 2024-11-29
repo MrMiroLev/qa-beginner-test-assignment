7e3488b2ed919b9b08b575b89e5bdc9b7f6c2e5d
Lev Kuzmin <levbaladev@gmail.com>
2024-11-29 12:52:28 +0530

    Add method getPosts2()

--- src/tests/class.test.ts
+++ src/tests/class.test.ts
@@ -3,30 +3,36 @@ import fs from 'fs';
 import path from 'path';
 
 const getEnvValueForKey = (key: string) => {
-    const filePath = path.join(__dirname, '../../env.json');
-    const fileContent = fs.readFileSync(filePath, 'utf-8');
-    const parsedContent = JSON.parse(fileContent);
-    return parsedContent[key];
+  const filePath = path.join(__dirname, '../../env.json');
+  const fileContent = fs.readFileSync(filePath, 'utf-8');
+  const parsedContent = JSON.parse(fileContent);
+  return parsedContent[key];
 };
 
 describe('Class test', () => {
-    let testClass: Class;
-    const baseUrl = 'https://jsonplaceholder.typicode.com';
-    const timeoutSec = 1;
+  let testClass: Class;
+  const baseUrl = 'https://jsonplaceholder.typicode.com';
+  const timeoutSec = 1;
 
-    beforeEach(() => {
-        testClass = new Class(baseUrl, timeoutSec);
+  beforeEach(() => {
+    testClass = new Class(baseUrl, timeoutSec);
+  });
+
+  afterEach(() => {
+    jest.clearAllMocks();
+  });
+
+  describe('Tests', () => {
+    it('[TEST_1] should fetch post data successfully', async () => {
+      const response = await testClass.getPosts1();
+
+      expect(response.id).toEqual(getEnvValueForKey('EXPECTED_ID_TEST_1'));
     });
 
-    afterEach(() => {
-        jest.clearAllMocks();
-    });
+    it('[TEST_2] should fetch post data successfully', async () => {
+      const response = await testClass.getPosts2();
 
-    describe('Tests', () => {
-        it('[TEST_1] should fetch post data successfully', async () => {
-            const response = await testClass.getPosts1();
-
-            expect(response.id).toEqual(getEnvValueForKey('EXPECTED_ID_TEST_1'));
-        });
+      expect(response.id).toEqual(getEnvValueForKey('EXPECTED_ID_TEST_2'));
     });
+  });
 });
